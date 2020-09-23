6c021a109319f38f9bed89d178fb6aa93e1cae02
KidHxq <1023629477@qq.com>
2020-09-23 17:54:19 +0800

    Initial commit

---
+++ src/com/idea/test2.java
@@ -0,0 +1,8 @@
+package com.idea;
+
+/**
+ * @author xiaoqin.huang
+ * @create 2020-09-17 12:19
+ */
+public class test2 {
+}
---
+++ src/test.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+/**
+ *
+ */
+public class test {
+
+    private  String s;
+
+
+
+}
